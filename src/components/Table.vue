cd9a61a1bbc7962d61b8629c19bd020779b73c7b
yattdev <yattdeveloper@gmail.com>
2022-01-22 18:03:50 +0100

    CReate a new component name Table, which content feature table

---
+++ src/components/Table.vue
@@ -0,0 +1,44 @@
+<template>
+    <MDBTable class="w-75 fs-4">
+      <tr class="table-dark">
+        <th class="px-4">FEATURES</th>
+        <th>STATE</th>
+      </tr>
+      <tr class="table-success">
+        <td class="px-4">Syntax checks</td>
+        <td>DONE</td>
+      </tr>
+      <tr class="table-success">
+        <td class="px-4">MX(Mail Exchange records) verification</td>
+        <td>DONE</td>
+      </tr>
+      <tr class="table-success">
+        <td class="px-4">Email Handler verification</td>
+        <td>DONE</td>
+      </tr>
+      <tr class="table-success">
+        <td class="px-4">Caching domain lookups to improve performance</td>
+        <td>DONE</td>
+      </tr>
+      <tr class="table-light">
+        <td class="px-4">Multiples emails verifications(e.g: emails.csv)</td>
+        <td>Comming soon</td>
+      </tr>
+      <tr class="table-success">
+        <td class="px-4">Supports asyncio for concurrency</td>
+        <td>DONE</td>
+      </tr>
+  </MDBTable>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { MDBTable } from 'mdb-vue-ui-kit';
+
+export default defineComponent({
+  name: 'Table',
+  components: {
+    MDBTable,
+  }
+})
+</script>
