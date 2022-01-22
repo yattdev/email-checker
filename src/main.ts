e85ba80e8748604fa89b59b3e84de2a15b03398d
yattdev <yattdeveloper@gmail.com>
2022-01-22 16:44:41 +0100

    Update main to import mdbootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "mdb-vue-ui-kit/css/mdb.min.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
@@ -5,6 +6,8 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import "mdb-vue-ui-kit/css/mdb.min.css";
+
 // import { BootstrapVue, IconsPlugin, PaginationPlugin } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
